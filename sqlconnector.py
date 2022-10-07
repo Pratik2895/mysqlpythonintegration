d38d9eaa630749717b7ad5195228ab3124879772
Pratik Bhikadiya <102245989+Pratik2895@users.noreply.github.com>
2022-10-07 21:34:27 +0530

    Add files via upload

---
+++ sqlconnector.py
@@ -0,0 +1,27 @@
+import mysql.connector
+
+try:
+    connection = mysql.connector.connect(host='localhost',
+                                         database='accenture',
+                                         user='pynative',
+                                         password='pynative@#29')
+
+    mySql_Create_Table_Query = """CREATE TABLE Laptop ( 
+                             Id int(11) NOT NULL,
+                             Name varchar(250) NOT NULL,
+                             Price float NOT NULL,
+                             Purchase_date Date NOT NULL,
+                             PRIMARY KEY (Id)) """
+
+    cursor = connection.cursor()
+    result = cursor.execute(mySql_Create_Table_Query)
+    print("Laptop Table created successfully ")
+
+except mysql.connector.Error as error:
+    print("Failed to create table in MySQL: {}".format(error))
+finally:
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
+        print("MySQL connection is closed")
+ 
